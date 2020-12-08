b47de5147acba3d4c5b3ed9c2ad023477d11ee77
Jerrod Styrk <jstyrk@citadel.edu>
2020-12-08 10:22:26 -0800

    Pass  first test
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

--- Triangle/Models/Triangle.cs
+++ Triangle/Models/Triangle.cs
@@ -4,7 +4,14 @@ namespace Tracker
   {
     public bool IsTriangle(int side1, int side2, int side3)
     {
-      return false;
+      if ((side1 == side2) && (side1 == side3))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
